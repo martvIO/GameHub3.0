5fa100cb6bb9146f1318bc9dfaeee8d77ae48481
martvIO <136852336+martvIO@users.noreply.github.com>
2024-12-13 00:20:09 +0200

    updated

--- GameHub/src/components/Games/number-guessing/Controls.tsx
+++ GameHub/src/components/Games/number-guessing/Controls.tsx
@@ -7,18 +7,18 @@ interface InputControlsProps {
 }
 
 export const InputControls: React.FC<InputControlsProps> = ({ guess, setGuess }) => {
-  const adjustGuess = (value: number) => setGuess((prev) => prev + value);
+  const setGuesserValue = (e: React.ChangeEvent<HTMLInputElement>) => setGuess(Number(e.target.value));
 
   return (
     <div className="flex items-center justify-center mb-4">
-      <Buttons KeyLists={[-1,-5,-10]} setGuess={setGuess}></Buttons>
+      <Buttons KeyLists={[-1,-5,-10]} setGuess={setGuess}/>
       <motion.input
         type="number"
         value={guess}
-        onChange={(e) => setGuess(Number(e.target.value))}
+        onChange={(e) => setGuesserValue(e)}
         className="w-20 p-2 text-center text-black dark:text-gray-900 border border-gray-400 dark:border-gray-700 focus:outline-none appearance-none"
       />
-      <Buttons KeyLists={[1,5,10]} setGuess={setGuess}></Buttons>
+      <Buttons KeyLists={[1,5,10]} setGuess={setGuess}/>
     </div>
   );
 };
