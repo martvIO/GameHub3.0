8e631be8cd6da7e85a1724cdb6cd796ab3153768
martvIO <136852336+martvIO@users.noreply.github.com>
2025-06-17 18:13:07 +0300

    finall

--- Backend/app/utils.py
+++ Backend/app/utils.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 
 # Load .env file
-load_dotenv(r'C:\Users\martv\test\GameHub3.0\Backend\.env')
+load_dotenv(r'C:\Users\martv\Documents\GitHub\GameHub3.0\Backend\app\.env')
 
 # get environment variable
 def get_env_variable(name: str) -> str:
